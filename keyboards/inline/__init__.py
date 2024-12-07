432126270b22726ba3821fa3b94215596e47b0bf
Vladimir Kovalchuk <vova.kovalchuk.2003@gmail.com>
2024-12-07 13:22:50 +0200

    feat: permanent

--- keyboards/inline/__init__.py
+++ keyboards/inline/__init__.py
@@ -2,5 +2,6 @@ from aiogram import Router
 
 from .choose_servise_kb import servise_inline
 from .admin_menu_kb import admin_inline
+from .permanent_kb import permanent_keyboard
 
-__all__ = ['servise_inline',admin_inline]
+__all__ = ['servise_inline',admin_inline,permanent_keyboard]
